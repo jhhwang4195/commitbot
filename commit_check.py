40dae0309bf92e2aefe362184dd6b9af5cedb56c
jhhwang <jhhwang4195@gmail.com>
2018-01-08 21:06:33 +0900

    Fixed pep8 syntax

--- commit_check.py
+++ commit_check.py
@@ -10,24 +10,28 @@ local_tz = pytz.timezone('Asia/Seoul')
 
 token = ""
 try:
-	token = os.environ['SLACK_BOOT_TOKEN']
+    token = os.environ['SLACK_BOOT_TOKEN']
 except Exception as err:
-	print ("[Error] %s" % (str(err)))
+    print ("[Error] %s" % (str(err)))
 
 slack = Slacker(token)
 channels = ['#dailycommit']
 
+
 def post_to_channel(message):
     slack.chat.post_message(channels[0], message)
+
 
 def get_repo_last_commit_delta_time(owner, repo):
     repo = github3.repository(owner, repo)
     return repo.pushed_at.astimezone(local_tz)
 
+
 def get_delta_time(last_commit):
     now = datetime.datetime.now(local_tz)
     delta = now - last_commit
     return delta.days
+
 
 def main():
     members = (
@@ -46,6 +50,7 @@ def main():
             reports.append('*%s* has not committed for *%s* days.' % (name, delta_time))
 
     post_to_channel('\n A bot that checks daily commits.\n' + '\n'.join(reports))
+
 
 if __name__ == '__main__':
     main()
